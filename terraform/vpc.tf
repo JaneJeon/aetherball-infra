526a437fd37ec79943c1c43a87c9b5b999d9cbbe
Jane Jeon <git@janejeon.com>
2025-08-02 17:03:47 -0700

    Fix subnetwork error

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,9 +6,10 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name    = "${var.project_id}-subnetwork"
-  region  = var.region
-  network = google_compute_network.vpc.name
+  name          = "${var.project_id}-subnetwork"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
 
   stack_type       = "IPV4_IPV6"
   ipv6_access_type = "EXTERNAL"
